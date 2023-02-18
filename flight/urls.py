60da5befee6d994cca47968edf5d91ebae8039ef
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-18 13:54:31 +0300

    f.urls

--- flight/urls.py
+++ flight/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import FlightView, ReservationView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register("flights", FlightView)
-router.register("reservations", ReservationView)
+
 
 urlpatterns = [
     
